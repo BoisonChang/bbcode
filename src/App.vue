cba4ba592a8ffc37063aded42c7190e6c426fae2
BoisonChang <dragoncres@gmail.com>
2022-09-15 15:15:02 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Message :html="bbcode" />
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { ref } from 'vue';
+import Message from './components/MessageArea';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+const bbcode = ref(
+  'Hi #smile#, we are Code Zero #smile#shy#.\n' +
+  'Happy `Coding` #heart#.\n' +
+  '`#smile#` :)'
+);
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/MessageArea.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+    <h2 for="phone">BBCode</h2><br>
+    <textarea v-model="input" @input="changeHandler"></textarea>
+    <h2>HTML</h2><br>
+    <p type="text" v-html="output"></p>｀
+</div>
+</template>
+
+
+<script setup>
+  /* eslint-disable */
+  import { defineProps, ref } from "vue"
+  defineProps({
+    html: String,
+  });
+  let input = ref(), output = ref()
+  const changeHandler = e => {
+    const bold = /\[b\]([^\[\/b\]]+)\[\/b\]/g, code = /`([^`]+)`/g
+    const smile =/\#smile\#/g, smileshy =/\#smile\#shy\#/g, heart =/\#heart\#/g
+    output.value = e.target.value
+    .replace(code, `<pre>$1</pre>`)
+    .replace(bold, `<strong>$1</strong>`)
+    .replace(smileshy, `<span>&#128522</span>`)
+    .replace(smile, `<span>&#128512</span>`)
+    .replace(heart, `<span>🧡</span>`)
+  }
+</script>
